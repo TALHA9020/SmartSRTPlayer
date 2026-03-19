d68c87f0311bc7af1e9b8f9cc5f4b8434d5c8722
TALHA9020 <talhasajidabbasi90209050@gmail.com>
2026-03-19 19:25:37 +0500

    Update DataModels.kt

--- app/src/main/java/com/smart/srtplayer/DataModels.kt
+++ app/src/main/java/com/smart/srtplayer/DataModels.kt
@@ -1,20 +1,9 @@
 package com.smart.srtplayer
 
-import android.net.Uri
 import java.io.Serializable
 
 data class SubtitleItem(
     val startTime: Long,
     val endTime: Long,
     val text: String
-) : Serializable
-
-data class PlayerSettings(
-    val fontSize: Float,
-    val timerSize: Float,
-    val bgColor: Int,
-    val textColor: Int,
-    val opacity: Float,
-    val srtUri: String?,
-    val fontUri: String?
 ) : Serializable
